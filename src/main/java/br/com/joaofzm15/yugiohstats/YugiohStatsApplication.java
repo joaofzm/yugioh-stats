84d8c64bd88dfdbbff7eb88902be6f82f2e7a0f3
joao <joao.fzm@hotmail.com>
2022-03-21 01:02:34 -0300

    Project created

---
+++ src/main/java/br/com/joaofzm15/yugiohstats/YugiohStatsApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaofzm15.yugiohstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugiohStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YugiohStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaofzm15/yugiohstats/YugiohStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaofzm15.yugiohstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YugiohStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
