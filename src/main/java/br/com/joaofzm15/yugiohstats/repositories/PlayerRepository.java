b44a070edf062e719bfcdd98a070b7343ca61256
joao <joao.fzm@hotmail.com>
2022-03-22 03:15:54 -0300

    Add findAll and FindById methods to Player

--- src/main/java/br/com/joaofzm15/yugiohstats/repositories/PlayerRepository.java
+++ src/main/java/br/com/joaofzm15/yugiohstats/repositories/PlayerRepository.java
@@ -1,9 +1,11 @@
 package br.com.joaofzm15.yugiohstats.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.joaofzm15.yugiohstats.entitites.Player;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
 	//No need to instantiate methods, they're inherited from JpaRepository
--- src/main/java/br/com/joaofzm15/yugiohstats/resources/PlayerResource.java
+++ src/main/java/br/com/joaofzm15/yugiohstats/resources/PlayerResource.java
@@ -1,22 +1,36 @@
 package br.com.joaofzm15.yugiohstats.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.joaofzm15.yugiohstats.entitites.Player;
+import br.com.joaofzm15.yugiohstats.services.PlayerService;
 
 //RequestMapping value is the URL to access this
 @RestController
 @RequestMapping(value = "/players")
 public class PlayerResource {
 	
+	@Autowired
+	private PlayerService service;
+	
 	//Returns answers of web requisitions
 	@GetMapping
-	public ResponseEntity<Player> findAll(){
-		Player p = new Player(1L, "Revolver");
-		return ResponseEntity.ok().body(p);
+	public ResponseEntity<List<Player>> findAll(){
+		List<Player> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Player> findById(@PathVariable Long id){
+		Player obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
